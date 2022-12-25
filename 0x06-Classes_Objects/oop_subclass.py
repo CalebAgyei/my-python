212acff532edd5f95aea3302a9aabc1bb581bcc4
CalebAgyei <calebnagyei@gmail.com>
2022-12-25 05:22:44 -0800

    Inheritance updates

--- 0x06-Classes_Objects/oop_subclass.py
+++ 0x06-Classes_Objects/oop_subclass.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 class SchoolMember:
     """Represents a school member."""
 
@@ -5,6 +7,11 @@ class SchoolMember:
         """Initializes School member."""
         self.name = name
         self.age = age
+        print('(Initialized SchoolMember: {})'.format(self.name))
+
+    def tell(self):
+        """Tell my details."""
+        print('Name: "{}", Age: "{}",'.format(self.name, self.age), end=" ")
 
 
 class Teacher(SchoolMember):
@@ -14,22 +21,53 @@ class Teacher(SchoolMember):
         """Initializes Teacher."""
         SchoolMember.__init__(self, name, age)
         self.salary = salary
+        print('(Initialized Teacher: {})'.format(self.name))
 
+    def tell(self):
+        SchoolMember.tell(self)
+        print('My salary: "{}"'.format(self.salary))
 
 
 class Student(SchoolMember):
     """Represents a Student."""
 
-    def __init__(self, name, age, marks):
+    def __init__(self, name, age, score):
         """Initializes a Student."""
         SchoolMember.__init__(self, name, age)
-        self.marks = marks
+        self.score = score
+        print('(Initialized Student: {})'.format(self.name))
+
+    def tell(self):
+        SchoolMember.tell(self)
+        print('Class score: "{}"'.format(self.score))
 
 
 class Senior(Student):
     """Represents a Senior."""
 
-    def __init__(self, name, age, marks, team):
+    def __init__(self, name, age, score, team):
         """Initializes a Senior student."""
-        Student.__init__(self, name, age, marks)
+        Student.__init__(self, name, age, score)
         self.team = team
+        print('(Initialized Senior: {})'.format(self.name))
+
+    def tell(self):
+        Student.tell(self)
+        print('Team: "{}"'.format(self.team))
+
+
+print("=============")
+t = Teacher("Caleb", 32, 10000)
+print("=============")
+s = Student("Bertha", 28, 3500)
+
+
+print("=============")
+t.tell()
+print("=============")
+s.tell()
+
+print("=============")
+members = [t, s]
+for member in members:
+    member.tell()
