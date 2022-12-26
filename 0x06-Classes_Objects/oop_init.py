5e93d2700130a962a8d2e2dca77f133c53e163da
CalebAgyei <calebnagyei@gmail.com>
2022-12-26 19:50:34 +0000

    Classes by Swaroop

---
+++ 0x06-Classes_Objects/oop_init.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def say_hi(self):
+        print('Hello, my name is', self.name)
+
+p = Person('Caleb')
+p.say_hi()
+# previous 2 lines could also be written as Person('Caleb').say_hi()
