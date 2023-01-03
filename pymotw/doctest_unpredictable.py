0bc8156020bf19751eee65a3d173c1f5e781e889
CalebAgyei <calebnagyei@gmail.com>
2023-01-03 09:34:51 +0000

    PYMOTW

---
+++ pymotw/doctest_unpredictable.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class MyClass:
+    pass
+
+def unpredictable(obj):
+    """Returns a new list containing obj.
+
+    >>> unpredictable(MyClass())
+    [<doctest_unpredictable.MyClass object at 0x10055a2d0>]
+    """
+    return (obj)
